54e6c752e891d09d4d4f565e2ed18847715c8bd3
Abhishek Kumar <abhishekism0010@gmail.com>
2025-09-08 07:23:41 +0000

    fix: added initial company type fixtures

--- nirmaan_crm/hooks.py
+++ nirmaan_crm/hooks.py
@@ -260,6 +260,7 @@ fixtures = [
     {"dt": "Role", "filters": [["role_name", "like", "Nirmaan %"]]},
     {"dt": "Role Profile", "filters": [["role_profile", "like", "Nirmaan %"]]},
     "Portal Menu Item",
+    "CRM Company Type"
 ]
 
 
